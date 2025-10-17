9493cf7226144ba8d1b4b9aba54ded6eeb178358
matheusassuncaoo <matheus.assuncaomt18@gmail.com>
2025-10-17 19:22:14 -0400

    first commit

---
+++ src/main/java/com/br/fasipe/fisioclin/FisioclinApplication.java
@@ -0,0 +1,13 @@
+package com.br.fasipe.fisioclin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisioclinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisioclinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fasipe/fisioclin/FisioclinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fasipe.fisioclin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisioclinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
